cef1befd0ad2d415237ac5559a130b4daa9e2970
mialyrabarison <mialyrabarison@gmail.com>
2024-12-14 16:28:38 +0300

    add commentlist

--- App.js
+++ App.js
@@ -1,15 +1,36 @@
-//creer une fonction et recuperer les élément des la HTML par id//
-function verifyallinput() {
-  const firstname = document.getElementById("first-name");
+
+function verifyallinput() { //Permet de verifier les formulaires vides)
+  const firstname = document.getElementById("first-name");//creer une fonction et recuperer les élément des la HTML par id//
   const lastname = document.getElementById("last-name");
   const message = document.getElementById("message");
   const error = document.getElementById("error-message");
   // Vérifier les conditions//
   if (firstname.value == "" || lastname.value == "" || message.value == "") {
     error.style.display = "block";
-  } // NONE#BLOCK//
+  } // NONE#BLOCK pour changer le style dans HTML//
   else {
     error.style.display = "none";
-  }
+    addelementtolistofcomment (firstname.value ,lastname.value , message.value) // appel la fonction addelement to assigne attRibut in fonction//
+    firstname.value = ""; //renitialiser les formulaires//
+    lastname.value  = ""; 
+    message.value = ""
+
 }
-//Vérifier les condition
+}
+
+
+function addelementtolistofcomment(firstname , lastname , message){
+    const commentlist = document.getElementById("comment-list");
+    const commentdiv = document.createElement("div")   // Creer une balise div HTML//
+    const newcomment =`<div class="flex space-x-4 text-sm text-gray-500">
+                <div class="flex-1 py-10 ">
+                  <h3 class="font-medium text-gray-900">${firstname} ${lastname}</h3>
+                  <div class="prose prose-sm mt-4 max-w-none text-gray-500">
+                    <p>${message}</p>
+                  </div>
+                </div>
+              </div>`
+    commentdiv.innerHTML = newcomment // Permet d'inserer un texte dans HTML//
+    commentlist.appendChild(commentdiv)   // APPENDDCHILD permet d'inserer un nouveau dans la classe mère//
+    
+}
